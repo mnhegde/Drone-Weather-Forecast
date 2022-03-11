6fc20f6065ba56fe5dd448c56959b2a7601d3707
Manu Hegde <manu.hegde02@gmail.com>
2022-03-10 22:36:12 -0500

    rendering for android

--- App/Pages/Data.js
+++ App/Pages/Data.js
@@ -20,9 +20,14 @@ export default function Data() {
     if (usingFt) setUsingFt(false)
     else setUsingFt(true)
   }
+
   function toggleKnots() {
     if (usingKnots) setUsingKnots(false)
     else setUsingKnots(true)
+  }
+
+  function fetchData() {
+    console.log("test");
   }
 
   function padding(a, b, c, d) {
@@ -58,7 +63,7 @@ export default function Data() {
 
   let renderLayers = layerData.map((data, i) => {
     return (<View centerH style={{ flexDirection: "column", marginTop: "2%" }}>
-      <Text onPress={null/*NEED A FUNCTION HERE TO UPDATE DATA */} center color='white' style={{ fontSize: 20, borderColor: "#8FD9FF", borderWidth: "2px", borderRadius: "9%", ...padding(10, 25, 10, 25), width: "35%" }}>{data}</Text>
+      <Text onPress={null/*NEED A FUNCTION HERE TO UPDATE DATA */} center color='white' style={{ fontSize: 20, borderColor: "#8FD9FF", borderWidth: 2, borderRadius: .09, ...padding(10, 25, 10, 25), width: "35%" }}>{data}</Text>
     </View>)
   })
   let renderCountries = countries.map((data, i) => {
@@ -70,9 +75,9 @@ export default function Data() {
         <Image width={"100%"} height={"100%"} resizeMode={"stretch"} source={require("../assets/BackgroundData.png")} key="Background" />
       </View>
       <Text color="white" center style={{ fontSize: 30, fontWeight: "bold", marginTop: "10%", marginBottom: "10%" }}>DroneScout</Text>
-      <View width={"95%"} backgroundColor="#5D94B0" style={{ flexDirection: "column", opacity: "85%", borderRadius: "15%" }}>
+      <View width={"95%"} backgroundColor="#5D94B0" style={{ flexDirection: "column", opacity: 85, borderRadius: 15 }}>
         {rainWarning && (
-          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: "15%" }}>
+          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: 15 }}>
             <View centerH style={{ marginTop: "2%", marginBottom: "2%", flexDirection: "row" }}>
               <Image source={require("../assets/Warning.png")} style={{ marginLeft: "4%", marginRight: "4%" }} resizeMethod="scale" key="warning" />
               <Text color="white" center style={{ fontSize: 20, fontWeight: "bold" }}>High chance of rain</Text>
@@ -96,9 +101,9 @@ export default function Data() {
         <Text color="white" style={{ fontSize: 10, marginLeft: "3%", marginBottom: "3%" }}>Source: Kanda Weather</Text>
 
       </View>
-      <View width={"95%"} backgroundColor="#5D94B0" style={{ opacity: "85%", borderRadius: "15%", marginTop: "5%" }}>
+      <View width={"95%"} backgroundColor="#5D94B0" style={{ opacity: 85, borderRadius: 15, marginTop: "5%" }}>
         {windWarning && (
-          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: "15%" }}>
+          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: 15 }}>
             <View centerH style={{ marginTop: "2%", marginBottom: "2%", flexDirection: "row" }}>
               <Image source={require("../assets/Warning.png")} style={{ marginLeft: "4%", marginRight: "4%" }} resizeMethod="scale" key="warning" />
               <Text color="white" center style={{ fontSize: 20, fontWeight: "bold" }}>High wind speeds</Text>
@@ -126,9 +131,9 @@ export default function Data() {
 
         <Text color="white" style={{ fontSize: 10, marginLeft: "3%", marginTop: "3%", marginBottom: "3%" }}>Source: Kanda Weather</Text>
       </View>
-      <View width={"95%"} backgroundColor="#5D94B0" style={{ opacity: "85%", borderRadius: "15%", marginTop: "5%" }}>
+      <View width={"95%"} backgroundColor="#5D94B0" style={{ opacity: 85, borderRadius: 15, marginTop: "5%" }}>
         {turbulenceWarning && (
-          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: "15%" }}>
+          <View width={"96%"} centerH backgroundColor="#DB9706" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: 15 }}>
             <View centerH style={{ marginTop: "2%", marginBottom: "2%", flexDirection: "row" }}>
               <Image source={require("../assets/Warning.png")} style={{ marginLeft: "4%", marginRight: "4%" }} resizeMethod="scale" key="warning" />
               <Text color="white" center style={{ fontSize: 20, fontWeight: "bold" }}>Turbulence detected</Text>
@@ -147,7 +152,7 @@ export default function Data() {
       </View>
       {
         floodWarning && (
-          <View width={"96%"} centerH backgroundColor="#BF414F" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: "15%" }}>
+          <View width={"96%"} centerH backgroundColor="#BF414F" style={{ marginLeft: "2%", marginRight: "2%", marginTop: "2%", marginBottom: "2%", flexDirection: "row", borderRadius: 15 }}>
             <View centerH style={{ marginTop: "2%", marginBottom: "2%", flexDirection: "row" }}>
               <Image source={require("../assets/Alert.png")} style={{ marginLeft: "4%", marginRight: "4%" }} resizeMethod="scale" key="warning" />
               <Text color="white" center style={{ fontSize: 20, fontWeight: "bold" }}>High flood risk</Text>
@@ -157,7 +162,7 @@ export default function Data() {
         )
       }
 
-      <View width={"100%"} centerV backgroundColor="white" style={{ marginTop: "auto", borderRadius: "15%" }} height={"10%"} >
+      <View width={"100%"} centerV backgroundColor="white" style={{ marginTop: "auto", borderRadius: 15 }} height={"10%"} >
         <View style={{ justifyContent: "space-between", flexDirection: "row", }} >
           <Text color="grey" style={{ fontSize: 35, fontWeight: "bold", marginLeft: "3%" }}>City, Country</Text>
           <View style={{ flexDirection: "row", marginRight: "3%", justifyContent: "space-between" }} width="20%">
@@ -174,7 +179,7 @@ export default function Data() {
       </View>
 
       <Dialog visible={search} onDismiss={() => setSearch(false)} panDirection={PanningProvider.Directions.DOWN}>
-        {<View width={"100%"} backgroundColor="white" style={{ borderRadius: "15%" }}>
+        {<View width={"100%"} backgroundColor="white" style={{ borderRadius: 15 }}>
           <View centerH style={{ flexDirection: "row", marginLeft: "3%", marginRight: "3%", borderBottomWidth: "2px", borderBottomColor: "#E6E6E6", borderBottomRadius: "5%" }}>
             <Image resizeMode={"stretch"} source={require("../assets/Search.png")} key="Search" />
             <Text color="grey" style={{ fontSize: 35, fontWeight: "bold", marginLeft: "3%" }}>Location Select</Text>
@@ -184,7 +189,7 @@ export default function Data() {
       </Dialog>
 
       <Dialog visible={settings} onDismiss={() => setSettings(false)} panDirection={PanningProvider.Directions.DOWN}>
-        {<View width={"100%"} backgroundColor="white" style={{ borderRadius: "15%" }}>
+        {<View width={"100%"} backgroundColor="white" style={{ borderRadius: 15 }}>
           <View centerH style={{ flexDirection: "row", marginLeft: "3%", marginRight: "3%", borderBottomWidth: "2px", borderBottomColor: "#E6E6E6", borderBottomRadius: "5%" }}>
             <Image resizeMode={"stretch"} source={require("../assets/Settings.png")} key="Settings" />
             <Text color="grey" style={{ fontSize: 35, fontWeight: "bold", marginLeft: "3%" }}>Settings</Text>
