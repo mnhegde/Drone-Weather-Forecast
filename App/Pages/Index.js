d1a0fd606641848904ec5f03e4a688e232431c19
Spencer Evans-Cole <spencer.ec09@gmail.com>
2022-03-15 08:30:11 -0400

    Handling edge case on index

--- App/Pages/Index.js
+++ App/Pages/Index.js
@@ -7,6 +7,7 @@ import SelectDropdown from 'react-native-select-dropdown'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function Index() {
     const [cities, setCities] = useState({})
+    const [canChange, setCanChange] = useState(false)
     //const [selectedItem, setSelectedItem] = useState();
     useEffect(() => {
         fetch("http://20.90.82.229:5000/cities")
@@ -19,6 +20,11 @@ export default function Index() {
                 setCities(obj)
             })
     }, [])
+    async function selected(city){
+        await AsyncStorage.setItem("city",city)
+        setCanChange(true)
+        console.log(canChange)
+    }
     return (
         <View flex center width={"100%"} height="100%">
             <View style={{ position: "absolute" }} width={"100%"} height={"100%"} >
@@ -28,10 +34,12 @@ export default function Index() {
             <Text color="white" center style={{ fontSize: 50, fontWeight: "bold" }}>DroneScout</Text>
             <Text center color="white" style={{ fontSize: 15 }}>Scout the weather before you take flight!</Text>
             <View backgroundColor='white' height={"5%"} center style={{ marginTop: "5%", borderRadius: 20, flexDirection: "row", justifyContent: "space-between" }}>
-                <SelectDropdown rowTextStyle={{color: "#707070"}} dropdownStyle={{borderRadius: 20}} buttonTextStyle={{color: "#707070"}} buttonStyle={{backgroundColor: "white", height: "70%", borderRadius: 20, width: "70%"}}  data={cities} onSelect={(selectedItem, index) => {AsyncStorage.setItem("city",selectedItem)}}/>
-                <Link to="/data">
-                    <Image style={{ marginRight: "2%", }} source={require("../assets/arrow.png")} key="arrow" />
-                </Link>
+                <SelectDropdown rowTextStyle={{ color: "#707070" }} dropdownStyle={{ borderRadius: 20 }} buttonTextStyle={{ color: "#707070" }} buttonStyle={{ backgroundColor: "white", height: "70%", borderRadius: 20, width: "70%" }} data={cities} onSelect={(selectedItem, index) => {selected(selectedItem)}} />
+                {canChange && (
+                    <Link to="/data">
+                        <Image style={{ marginRight: "2%", }} source={require("../assets/arrow.png")} key="arrow" />
+                    </Link>
+                )}
             </View>
         </View>
     )
